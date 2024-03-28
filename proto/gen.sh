0bb775ce18e742de03ae1fc5fa2a74c9721c49f9
Norihide Oda <60128781+Aosanori@users.noreply.github.com>
2024-03-28 21:40:43 +0900

    protoのディレクトリ変更

--- proto/gen.sh
+++ proto/gen.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-CLIENT_OUTDIR=client/src/grpc
-SERVER_OUTPUT_DIR=backend/api/chat
+CLIENT_OUTDIR=../client/src/chatroom
+SERVER_OUTPUT_DIR=../backend/api/chat
 
-mkdir -p ${CLIENT_OUTDIR} ${SERVER_OUTPUT_DIR}
+# mkdir -p ${CLIENT_OUTDIR} ${SERVER_OUTPUT_DIR}
 
 # protocol/helloworld.proto
 #  --js_out => helloworld_pb.js
 #  --grpc-web_out => helloworld_pb.d.ts
 #  --grpc-web_out => HelloworldServiceClientPb.ts
 #  --go_out => helloworld.pb.go
-protoc proto/chat.proto \
+
+protoc chat.proto \
     --js_out=import_style=commonjs:${CLIENT_OUTDIR} \
     --grpc-web_out=import_style=typescript,mode=grpcwebtext:${CLIENT_OUTDIR} \
-    --go_out=${SERVER_OUTPUT_DIR} --go_opt=paths=source_relative
+    --go_out=${SERVER_OUTPUT_DIR} --go_opt=paths=source_relative \
+    --go-grpc_out=${SERVER_OUTPUT_DIR} --go-grpc_opt=paths=source_relative 
