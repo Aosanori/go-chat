2cb4c25cbcc8f39f1d84c46352de5e849b7639e5
Norihide Oda <60128781+Aosanori@users.noreply.github.com>
2024-03-28 17:02:04 +0900

    Docker環境整備

--- gen.sh
+++ gen.sh
@@ -10,7 +10,7 @@ mkdir -p ${CLIENT_OUTDIR} ${SERVER_OUTPUT_DIR}
 #  --grpc-web_out => helloworld_pb.d.ts
 #  --grpc-web_out => HelloworldServiceClientPb.ts
 #  --go_out => helloworld.pb.go
-protoc --proto_path=protocol helloworld.proto \
+protoc proto/chat.proto \
     --js_out=import_style=commonjs:${CLIENT_OUTDIR} \
     --grpc-web_out=import_style=typescript,mode=grpcwebtext:${CLIENT_OUTDIR} \
-    --go_out=plugins=grpc:${SERVER_OUTPUT_DIR}
+    --go_out=${SERVER_OUTPUT_DIR} --go_opt=paths=source_relative
