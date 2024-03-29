8b3ab5ba4047177ed3b218e1a287e35980abd764
Norihide Oda <60128781+Aosanori@users.noreply.github.com>
2024-03-29 17:14:04 +0900

    redisのPub/Sub機能を利用してメッセージに関するイベントを管理

--- backend/api/chat/chat_server.go
+++ backend/api/chat/chat_server.go
@@ -2,13 +2,18 @@ package chat
 
 import (
 	context "context"
+	"encoding/json"
 	"fmt"
+	"log"
 
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
+
 	// chatpb "backend/api/chat"
+	db "backend/db"
 )
 
+const RoomId = "0"
 var messages []*Message = []*Message{}
 var timestamps []*timestamppb.Timestamp = []*timestamppb.Timestamp{}
 
@@ -21,21 +26,41 @@ func NewChatServer() *ChatServer {
 }
 
 func (s *ChatServer) CreateMessage (ctx context.Context, req *CreateMessageRequest) (*CreateMessageResponse, error) {
+	client := db.NewRedisClient()
+	message, _ := json.Marshal(req)
+	_ = client.Publish(ctx, req.Content.RoomId, message).Err()
+
 	messages = append(messages, req.Content)
 	timestamps = append(timestamps, timestamppb.Now())
 	return &CreateMessageResponse{
-		// Result: fmt.Sprintf("%s\n", messages),
 		Result: fmt.Sprintf("Success ! This chatroom has %d messages.", len(messages)),
 	}, nil
 }
 
+// 一度目のアクセスで保持しているメッセージを流し、それ以降は、新しいメッセージを検知したときのみデータを送る
 func (s *ChatServer) GetMessageStream (_ *emptypb.Empty, stream ChatService_GetMessageStreamServer) error {
-	for {
-		// レスポンスの生成
-		if err := stream.Send(&MessagesResponse{Contents: messages, Timestamps: timestamps}); err != nil {
-			return err
-		}
+	client := db.NewRedisClient()
+	pubsub := client.Subscribe(stream.Context(), RoomId)
+	defer pubsub.Close()
+	_, err := pubsub.Receive(stream.Context())
+	if err != nil {
+			log.Fatal(err)
 	}
+
+	ch := pubsub.Channel()
+	// Consume messages.
+	for msg := range ch {
+			message := MessagesResponse{Contents: messages, Timestamps: timestamps};
+			err := json.Unmarshal([]byte(msg.Payload), &message)
+			if err != nil {
+					log.Fatal(err)
+			}
+			fmt.Println(msg.Channel, msg.Payload)
+			if err := stream.Send(&message); err != nil {
+					return err
+			}
+	}
+	return nil
 }
 
 // // 一度目のアクセスで保持しているメッセージを流し、それ以降は、新しいメッセージを検知したときのみデータを送る
--- backend/db/redis.go
+++ backend/db/redis.go
@@ -4,7 +4,7 @@ import "github.com/redis/go-redis/v9"
 
 func NewRedisClient() *redis.Client {
     client := redis.NewClient(&redis.Options{
-        Addr:     "localhost:6379",
+        Addr:     "redis:6379",
         Password: "", // no password set
         DB:       0,  // use default DB
     })
